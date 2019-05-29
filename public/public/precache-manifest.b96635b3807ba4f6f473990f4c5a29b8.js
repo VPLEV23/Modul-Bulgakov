5b3f9b852e1bf9bc92060af093b6a68bdcfccf24
Vlad <bladetheblade12@gmail.com>
2019-05-29 09:43:27 +0200

    modul

---
+++ public/public/precache-manifest.b96635b3807ba4f6f473990f4c5a29b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2b84b5c7a69cd7b55dcf370993665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29abc227c3671ef308ce",
+    "url": "/static/js/2.3a4d9627.chunk.js"
+  },
+  {
+    "revision": "a34c9b2801a45373b009",
+    "url": "/static/js/main.4daac71f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
